781b78c8f34eb5ac9b22fd915aacfe55da1344fb
OwenMac <owen4123@gmail.com>
2024-01-11 22:03:55 +0800

    Able to use Clerk as Auth to sign up and log in

---
+++ app/(auth)/dist/layout.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.metadata = void 0;
+exports.metadata = {
+    title: 'Threads',
+    description: 'A Next.js 13 Meta Threads Application'
+};
+function RootLayout(_a) {
+    var children = _a.children;
+}
+exports["default"] = RootLayout;
